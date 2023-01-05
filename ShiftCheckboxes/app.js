8818d07b81419869355d87a02ae15d95b47ee9f2
merkaya1 <erkaya.mustafa1@gmail.com>
2023-01-05 20:32:47 +0300

    Hold Shift and Checked

---
+++ ShiftCheckboxes/app.js
@@ -0,0 +1,28 @@
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+console.log(checkboxes)
+
+let lastChecked;
+
+function handleCheck(e) {
+    console.log(e.shiftKey)
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+     
+      checkboxes.forEach(checkbox => {
+        console.log(checkbox);
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+          console.log('Starting to check them in between!');
+        }
+        if(inBetween) {
+            checkbox.checked = true
+        }
+       
+      });
+    }
+  
+    lastChecked = this;
+  }
+
+checkboxes.forEach((box) => box.addEventListener('click', handleCheck))
